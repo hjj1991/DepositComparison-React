f62faefd65127e583a405f8d82f8830b492cd095
JaeJeong <hjj19911@naver.com>
2019-10-14 17:44:35 +0900

    first

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import {render} from 'react-dom';
+// import logo from '../logo.png' //실제 로고파일 경로
+
+function Example() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+  
+
+
+function Login() {
+    render(<Example />);
+}
+  
+export default Login;
---
+++ src/store/modules/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+
+export default combineReducers({
+  // 다른 리듀서를 만들게되면 여기에 넣어줌..
+});
